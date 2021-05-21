cdb1167ac41ff1c6b561fdd25912c9268b17c05a
namdq_tt <namdq_tt@rikkeisoft.com>
2021-05-21 15:08:20 +0700

    Connect firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUamkQSbsEE1drN4T_cK1kq9XBIt3kCHs",
+  authDomain: "itss-todo-app.firebaseapp.com",
+  projectId: "itss-todo-app",
+  storageBucket: "itss-todo-app.appspot.com",
+  messagingSenderId: "352581114710",
+  appId: "1:352581114710:web:3fc3843f07be55495f94f7",
+};
+
+firebase.initializeApp(firebaseConfig);
